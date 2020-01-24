64e576d91a27a8cadaca2c8faf7617d93942b542
Bryan Ayala <brianwithay56@gmail.com>
2020-01-24 13:09:38 -0800

    Initial Commit

---
+++ android/app/src/main/kotlin/com/edu/cpp/cs/cs4990/dash_n_dine/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.edu.cpp.cs.cs4990.dash_n_dine
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
