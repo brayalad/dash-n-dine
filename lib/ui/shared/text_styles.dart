0d7b1596d99b3783bfb77a317ed131ddc7a74097
Bryan Ayala <brianwithay56@gmail.com>
2020-02-21 15:44:23 -0800

    Added shared TextStyles

---
+++ lib/ui/shared/text_styles.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+final TextStyle headerStyle2 = TextStyle(
+		fontSize: 24,
+		fontWeight: FontWeight.w900
+);
+
+final TextStyle subHeaderStyle = TextStyle(
+		fontSize: 16.0,
+		fontWeight: FontWeight.w500
+);
+
+final TextStyle cardTitleStyle = TextStyle(
+		fontSize: 16.0,
+		fontWeight: FontWeight.w500
+);
+
+final TextStyle subCardTitleStyle = TextStyle(
+		fontSize: 20.0,
+		fontWeight: FontWeight.w700
+);
+
+final TextStyle headerStyle3 = TextStyle(
+		fontSize: 20.0,
+		fontFamily: 'Timesroman',
+		fontWeight: FontWeight.bold
+);
+
+
+final TextStyle subHintTitle = TextStyle(
+		fontFamily: 'Quicksand',
+		color: Colors.grey,
+		fontSize: 14.0
+);
+
+final TextStyle arialTheme = TextStyle(
+		color: Colors.white,
+		fontSize: 18,
+		letterSpacing: 1.4,
+		fontFamily: "arial"
+);
+
+final TextStyle textTheme = TextStyle(
+		fontSize: 16,
+		letterSpacing: 1.1,
+		fontFamily: "arial"
+);
+
+final TextStyle appBarTextTheme = TextStyle(
+		fontSize: 18.0,
+		fontWeight: FontWeight.w600
+) ;
